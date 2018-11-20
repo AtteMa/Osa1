cf8b27677b20561a06e22ef3867a9b7a62ef15f5
AtteMa <43059427+AtteMa@users.noreply.github.com>
2018-11-20 16:52:15 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Hello = () => {
+    return (
+      <div>
+        <p>Hello {props.name}, you are {props.age} years old</p>
+      </div>
+    )
+  }
+
+const App = () => {
+    const nimi = 'Pekka'
+    const ika = 10
+    return(
+    <div>
+        <h1>Greetings</h1>
+        <Hello name="Arto" age={26 + 10} />
+        <Hello name={nimi} age={ika} />
+    </div>
+    )}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+
