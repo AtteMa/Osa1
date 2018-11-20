c2428c6dc52c31096b6772beef0c9c000ad06e96
AtteMa <43059427+AtteMa@users.noreply.github.com>
2018-11-20 18:20:09 +0200

    tehtävät 1.6-1.11

--- src/index.js
+++ src/index.js
@@ -1,75 +1,140 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function summa(a, b, c) {
-  return a + b + c
+function pyoristaja(luku, tarkkuus) {
+    var factor = Math.pow(10, tarkkuus);
+    return Math.round(luku * factor) / factor;
+}
+
+function keskiarvo(a, b, c) {
+    return pyoristaja((a - c) / (a + b + c), 1)
+}
+
+function positiivisia(a, b, c) {
+    return pyoristaja((a / (a + b + c)) * 100, 1)
 }
 
 const Otsikko = (props) => {
-  return (
-    <div>
-      <p>{props.kurssi.nimi}</p>
-    </div>
-  )
+    return (
+        <div>
+            <p>{props.unicafe}</p>
+        </div>
+    )
 }
 
-const Sisalto = (props) => {
-  return (
-    <div>
-      <Osa osa={props.kurssi.osat[0].nimi} tehtavia={props.kurssi.osat[0].tehtavia} />
-      <Osa osa={props.kurssi.osat[1].nimi} tehtavia={props.kurssi.osat[1].tehtavia} />
-      <Osa osa={props.kurssi.osat[2].nimi} tehtavia={props.kurssi.osat[2].tehtavia} />
-    </div>
-  )
+const tdStyle = {
+    padding: '0.2',
+    width: '100px'
 }
 
-const Yhteensa = (props) => {
-  return (
-    <div>
-      <p>yhteensä {summa(props.kurssi.osat[0].tehtavia, props.kurssi.osat[1].tehtavia, 
-        props.kurssi.osat[2].tehtavia)} tehtävää</p>
-    </div>
-  )
+const Statistic = (props) => {
+    return (
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <td style={tdStyle}>{props.unicafe}</td>
+                        <td>{props.counter}{props.percentage}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
-const Osa = (props) => {
-  return (
-    <div>
-      <p>{props.osa} {props.tehtavia}</p>
-    </div>
-  )
-}
-
-const App = () => {
-
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-      }
-    ]
-  }
-
-  return (
-    <div>
-      <Otsikko kurssi={kurssi} />
-      <Sisalto kurssi={kurssi} />
-      <Yhteensa kurssi={kurssi} />
-    </div>
-  )
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
 )
+
+const unicafe = {
+    otsikko: [
+        {nimi: 'anna palautetta'},
+        {nimi: 'statistiikka'}
+
+    ],
+    statistic: [
+        {nimi: 'hyvä',},
+        {nimi: 'neutraali',},
+        {nimi: 'huono',},
+        {nimi: 'keskiarvo',},
+        {nimi: 'positiivisia',}
+    ]
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            positive: 0,
+            neutral: 0,
+            negative: 0
+        }
+    }
+
+    asetaArvoon = (arvo, name) => {
+        return () => {
+            if (name == "positive") {
+                this.setState({ positive: arvo })
+            } else if (name == "neutral") {
+                this.setState({ neutral: arvo })
+            } else if (name == "negative") {
+                this.setState({ negative: arvo })
+            }
+        }
+    }
+    
+    display = (props) => {
+        return (
+            <div>
+                <Otsikko unicafe={unicafe.otsikko[0].nimi} />
+                <div>
+                    <Button handleClick={this.asetaArvoon(this.state.positive + 1, "positive")}
+                        text="hyvä"
+                    />
+                    <Button handleClick={this.asetaArvoon(this.state.neutral + 1, "neutral")}
+                        text="neutraali"
+                    />
+                    <Button handleClick={this.asetaArvoon(this.state.negative + 1, "negative")}
+                        text="huono"
+                    />
+                </div>
+                <Otsikko unicafe={unicafe.otsikko[1].nimi} />
+            </div>
+        )
+    }
+
+    Statistics = (props) => {
+        return (
+            <div>
+                <Statistic unicafe={unicafe.statistic[0].nimi} counter={this.state.positive} />
+                <Statistic unicafe={unicafe.statistic[1].nimi} counter={this.state.neutral} />
+                <Statistic unicafe={unicafe.statistic[2].nimi} counter={this.state.negative} />
+                <Statistic unicafe={unicafe.statistic[3].nimi} counter={keskiarvo(this.state.positive, this.state.neutral, this.state.negative)} />
+                <Statistic unicafe={unicafe.statistic[4].nimi} counter={positiivisia(this.state.positive, this.state.neutral, this.state.negative)} percentage={'%'} />
+            </div>
+        )
+    }
+
+    render() {
+        if (this.state.positive === 0 && this.state.neutral === 0 && this.state.negative === 0) {
+            return (
+                <div>
+                    <this.display />
+                    <p>ei yhtään palautetta annettu</p>
+                </div>
+            )
+        }
+        return (
+            <div>
+                <this.display />
+                <this.Statistics />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
