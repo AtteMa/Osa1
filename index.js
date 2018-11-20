92db528b55b4728d04b43a922e7f28bddc477c6b
AtteMa <43059427+AtteMa@users.noreply.github.com>
2018-11-20 18:25:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function summa(a, b, c) {
+  return a + b + c
+}
+
+const Otsikko = (props) => {
+  return (
+    <div>
+      <p>{props.kurssi.nimi}</p>
+    </div>
+  )
+}
+
+const Sisalto = (props) => {
+  return (
+    <div>
+      <Osa osa={props.kurssi.osat[0].nimi} tehtavia={props.kurssi.osat[0].tehtavia} />
+      <Osa osa={props.kurssi.osat[1].nimi} tehtavia={props.kurssi.osat[1].tehtavia} />
+      <Osa osa={props.kurssi.osat[2].nimi} tehtavia={props.kurssi.osat[2].tehtavia} />
+    </div>
+  )
+}
+
+const Yhteensa = (props) => {
+  return (
+    <div>
+      <p>yhteensä {summa(props.kurssi.osat[0].tehtavia, props.kurssi.osat[1].tehtavia, 
+        props.kurssi.osat[2].tehtavia)} tehtävää</p>
+    </div>
+  )
+}
+
+const Osa = (props) => {
+  return (
+    <div>
+      <p>{props.osa} {props.tehtavia}</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi} />
+      <Sisalto kurssi={kurssi} />
+      <Yhteensa kurssi={kurssi} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
